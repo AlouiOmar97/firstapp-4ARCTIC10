30e00710cb5d6952d2fef905863ddffbe1f63609
AlouiOmar97 <omar.aloui@esprit.tn>
2025-02-06 13:55:09 +0100

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'firstapp';
+export class AppComponent implements OnInit{
+  title = '4 ARCTIC 10';
+  search= "ABc"
+  nb = 5
+  y= true
+  x: String= "";
+  fillX(y:String ="Hi"): void {
+    this.x="Bonjour"
+  }
+
+  getNb(x: number){
+    return 10+x
+  }
+
+  ngOnInit(): void {
+    console.log("testt");
+    this.x="value 1"
+  }
 }
